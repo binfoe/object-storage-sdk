4724b4d5a055d8f16bb88502274b3c712613031e
abeyuhang <abeyuhang@gmail.com>
2024-02-29 18:14:08 +0800

    update minio size limit

--- src/minio.ts
+++ src/minio.ts
@@ -1,9 +1,8 @@
 import crypto from 'crypto';
-import { Transform } from 'stream';
 import type { ClientOptions } from 'minio';
 import { Client } from 'minio';
-import type { Readable, WriteObjectOptions, ReadObjectResult, BaseSDKConfig } from './common';
-import { EMPTY_OPTS, BaseObjectStorageSDK } from './common';
+import type { WriteObjectOptions, ReadObjectResult, BaseSDKConfig } from './common';
+import { Readable, EMPTY_OPTS, BaseObjectStorageSDK, TOO_LARGE_ERR } from './common';
 
 export type MinioSDKConfig = BaseSDKConfig & Exclude<ClientOptions, 'secretKey' | 'accessKey'>;
 
@@ -59,18 +58,49 @@ export class MinioObjectStorageSDK extends BaseObjectStorageSDK {
        * 代码逻辑不需要太严谨地考虑容错逻辑。
        */
       const hash = crypto.createHash('sha256');
-      const transform = new Transform();
+      const transform = new Readable({
+        read() {},
+      });
       let hashstr;
       req.on('data', (chunk) => {
-        transform.write(chunk);
+        transform.push(chunk);
         hash.update(chunk);
       });
       req.on('end', () => {
         hashstr = hash.digest('hex');
-        transform.end();
+        transform.push(null);
+      });
+      req.on('error', (err) => {
+        transform.destroy(err);
       });
       await client.putObject(this._config.bucket, filename, transform, meta);
       return hashstr;
+    } else if (options.limit) {
+      const LIMIT_SIZE = options.limit ?? 0;
+      const transform = new Readable({
+        read() {},
+      });
+      let total = 0;
+      let end = false;
+      req.on('data', (chunk) => {
+        if (end) return;
+        total += chunk.length;
+        if (total > LIMIT_SIZE) {
+          end = true;
+          transform.destroy(TOO_LARGE_ERR);
+          return;
+        }
+        transform.push(chunk);
+      });
+      req.on('end', () => {
+        if (end) return;
+        transform.push(null);
+        end = true;
+      });
+      req.on('error', (err) => {
+        transform.destroy(err);
+      });
+      await client.putObject(this._config.bucket, filename, transform, meta);
     } else {
       await client.putObject(this._config.bucket, filename, req, meta);
     }
